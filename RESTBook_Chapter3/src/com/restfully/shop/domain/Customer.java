b48f7da999f77dee77e1a1f562c08d6b6b815d20
SyCode7 <ktorkura@yahoo.com>
2014-12-12 14:00:34 +0100

    INITIAL

---
+++ RESTBook_Chapter3/src/com/restfully/shop/domain/Customer.java
@@ -0,0 +1,69 @@
+package com.restfully.shop.domain;
+
+public class Customer {
+	
+	private int  id;
+	private String  lastname;
+	private String  firstname;
+	private String  street;
+	private String  city;
+	private String  state;
+	private String  zip;
+	private String  country;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	
+	
+	
+
+}
