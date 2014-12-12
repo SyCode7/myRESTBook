500b69f40c482f8a5a85e9fd1d2189f31259f4a1
SyCode7 <ktorkura@yahoo.com>
2014-12-12 14:18:58 +0100

    resolved the customeresource conflict

--- RESTBook_Chapter3/src/com/restfully/shop/services/CustomerResource.java
+++ RESTBook_Chapter3/src/com/restfully/shop/services/CustomerResource.java
@@ -6,10 +6,16 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import com.restfully.shop.domain.Customer;
 
@@ -31,6 +37,17 @@ public class CustomerResource {
 		return Response.created(URI.create("/customers/" + customer.getId())).build();
 		
 	}
+	
+	@GET
+	@Path("id")
+	@Produces("application/xml")
+	
+	public StreamingOutput getCustomer(@PathParam("id") int id) {
+		return null;
+		
+		
+	}
+	
 
 	private Customer readCustomer(InputStream is) {
 		// TODO Auto-generated method stub
